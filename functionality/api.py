897c3868c7f5d38a2f2688358f0009f915284282
Matej Stražišar <matej.strazisar@outlook.com>
2024-01-04 19:33:26 +0100

    dodan API kljuc v skripti

--- functionality/api.py
+++ functionality/api.py
@@ -1,9 +1,12 @@
 from openai import OpenAI
-from .api_kljuc import CHATGPT_API_KLJUC
+# API kljuc za GPT API
+CHATGPT_API_KLJUC = "sk-xxxxx"
+
 
 client = OpenAI(api_key=CHATGPT_API_KLJUC)
 
-def poslji_gpt_api(sporocila):    
+
+def poslji_gpt_api(sporocila):
     completion = client.chat.completions.create(
         model="gpt-3.5-turbo",
         messages=sporocila
