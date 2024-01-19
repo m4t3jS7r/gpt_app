2538e7841b343d4a3f89ec321e6feefe013ed181
Matej Stražišar <matej.strazisar@outlook.com>
2024-01-19 15:20:36 +0100

    dodana podpora za napake pri klicu na API

--- functionality/api.py
+++ functionality/api.py
@@ -1,7 +1,7 @@
 import requests
 # API kljuc za GPT API
 
-CHATGPT_API_KLJUC = "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"
+CHATGPT_API_KLJUC = "sk-xxxxx"
 api_url = "https://api.openai.com/v1/chat/completions"
 headers = {
     "Content-Type": "application/json",
@@ -11,13 +11,15 @@ headers = {
 
 def poslji_gpt_api(sporocila):
 
-    data = {
-        "model": "gpt-3.5-turbo",
-        "messages": sporocila
-    }
-    response = requests.post(api_url, json=data, headers=headers)
-
-    if response.status_code == 200:
-        return response.json()["choices"][0]["message"]["content"]
-    else:
-        return "Klic na API ni uspel, poskusi znova?"
+    try:
+        data = {
+            "model": "gpt-3.5-turbo",
+            "messages": sporocila
+        }
+        response = requests.post(api_url, json=data, headers=headers)
+        if response.status_code == 200:
+            return response.json()["choices"][0]["message"]["content"]
+        else:
+            return f"Klic na API ni uspel: \n{response.json()['error']['message']}"
+    except requests.RequestException as e:
+        return f"Klic na API ni uspel: \n{e}"
